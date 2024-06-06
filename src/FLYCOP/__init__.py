e1f82f7f43f203aca6aadaae88961fca0080fd90
David San León Granado <dsanleon@gmail.com>
2024-06-06 12:16:32 +0200

    Modification to allow packaging

--- src/FLYCOP/__init__.py
+++ src/FLYCOP/__init__.py
@@ -3,5 +3,5 @@ from FLYCOP.FLYCOP import FLYCOP
 from FLYCOP.SimulatorFactory import SimulatorFactory
 from FLYCOP.OptimizerFactory import OptimizerFactory
 from FLYCOP.Consortia import Consortia
-from FLYCOP.Optimizer import Optimizer
+from FLYCOP.Optimizer import ParameterOptimizer
 from FLYCOP.Simulator import Simulator
